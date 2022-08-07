c6de36383b3ecba98592f155cf306e54316ba090
F1restorm07 <luke.noble.gardner@gmail.com>
2022-08-06 18:15:04 -0600

    08.06.22
    Former-commit-id: 3c064d0950b4fee0c5e8574860ec98a9f56627e8 [formerly c24b8903c49ec5d953f413c8d3037427a3f6457e]
    Former-commit-id: 0dcb0cbcbefc463660ace3cd8a42ef9e85fd5c30
    Former-commit-id: bf5ee9f6f685c3b124afdf1113aa12db20290bee
    Former-commit-id: ebf8386ac6f6517517934e39c30c5658272d6596
    Former-commit-id: 9f641d53cc0fa500b96db186a2087ac8864d4a82
    Former-commit-id: c545be461740322f9186e3fa2b1747b18eb17620
    Former-commit-id: b85e246e29e95e32208892d5335623ef1546e0a9
    Former-commit-id: 5fb5554a7b05c4bf699b87191498115a8880b710

--- eww/scripts/workspaces.sh
+++ eww/scripts/workspaces.sh
@@ -1,5 +1,36 @@
 #!/bin/sh
+# https://github.com/taylor85345/garden-hyprland-dotfiles/blob/master/hypr/garden/bar/scripts/workspace
 
-function workspaces {
-        if [[  ]]
+workspaces() {
+        unset -v \
+                o1 o2 o3 o4 o5 o6 o7 o8 o9 \
+                f1 f2 f3 f4 f5 f6 f7 f8 f9
+
+        for num in $(hyprctl workspaces | grep ID | awk '{print $3}'); do
+                export o"$num"="$num"
+        done
+
+        for num in $(hyprctl monitors | grep -B 4 "active: yes" | awk 'NR==1{print $3}'); do
+                export f"$num"="$num"
+        done
+
+        echo "(event box :onscroll \"echo {} | sed -e 's/up/-1/g' -e s/down/+1/g | xargs hyprctl dispatch workspace\" \
+                (box :class \"workspaces\" :orientation \"h\" :spacing 5 :spaces-evenly \"true\"                      \
+                        (button :onclick \"~/.config/hypr/workspace 1\" :class \"0$o1$f1\" \"\")\
+                        (button :onclick \"~/.config/hypr/workspace 2\" :class \"0$o2$f2\" \"\")\
+                        (button :onclick \"~/.config/hypr/workspace 3\" :class \"0$o3$f3\" \"\")\
+                        (button :onclick \"~/.config/hypr/workspace 4\" :class \"0$o4$f4\" \"\")\
+                        (button :onclick \"~/.config/hypr/workspace 5\" :class \"0$o5$f5\" \"\")\
+                        (button :onclick \"~/.config/hypr/workspace 6\" :class \"0$o6$f6\" \"\")\
+                        (button :onclick \"~/.config/hypr/workspace 7\" :class \"0$o7$f7\" \"\")\
+                        (button :onclick \"~/.config/hypr/workspace 8\" :class \"0$o8$f8\" \"\")\
+                        (button :onclick \"~/.config/hypr/workspace 9\" :class \"0$o9$f9\" \"\")\
+                )\
+        )"
 }
+
+workspaces
+
+socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r; do
+workspaces
+done
