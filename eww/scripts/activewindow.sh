c77d6eb5171bcd5dd13d21f17a0605d67a9cfa65
F1restorm07 <luke.noble.gardner@gmail.com>
2022-11-23 16:56:59 -0700

    11.23.22-4
    Former-commit-id: 996155e09048639a820240cd7402db29f25c08f8 [formerly e9ba2c83267d5e47655a18d471b427f76931df29]
    Former-commit-id: b491c22ca978f88ec2292a2bcdc7a1e792500ed7
    Former-commit-id: d0ffa21e2c4596c8dd4b4747be2e7f1cfad14b16
    Former-commit-id: a592f2a2e38a8c627ba95d2fd42b7eb892ec8b3c
    Former-commit-id: 6fa1f4bb2d6622d92a1c557b31a152e0db2101f2
    Former-commit-id: cbc1ace4601cde58fe00e7b7241d197387e629b0
    Former-commit-id: 482033370d381128a251b62b7b4e87e66a4e4e3b
    Former-commit-id: a86580d549124f012249d12062a99145447f716d

---
+++ eww/scripts/activewindow.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r; do
+        echo $(hyprctl activewindow | awk '/title:/ {$1=""; print $0}')
+done
