cce690f58cfb33cbd04180f00dc8224558ed8bcd
F1restorm <lukegardner@f1restorm-mac.local>
2023-06-19 21:51:21 -0600

    04.19.23-2

--- cmus/lib.pl
+++
@@ -1,7 +0,0 @@
-/Users/lukegardner/Downloads/audio/暗い自然 - 氷床 Earth/暗い自然 - 氷床 Earth - War Long Version.mp3
-/Users/lukegardner/Downloads/audio/暗い自然 - 氷床 Earth/暗い自然 - 氷床 Earth - 01 Snow Wasteland.mp3
-/Users/lukegardner/Downloads/audio/暗い自然 - 氷床 Earth/暗い自然 - 氷床 Earth - 02 まみれブラッド.mp3
-/Users/lukegardner/Downloads/audio/暗い自然 - 氷床 Earth/暗い自然 - 氷床 Earth - 03 Shards 冷凍スチール.mp3
-/Users/lukegardner/Downloads/audio/暗い自然 - 氷床 Earth/暗い自然 - 氷床 Earth - 04 人間の本能.mp3
-/Users/lukegardner/Downloads/audio/暗い自然 - 氷床 Earth/暗い自然 - 氷床 Earth - 05 草 Melt.mp3
-/Users/lukegardner/Downloads/audio/暗い自然 - 氷床 Earth/暗い自然 - 氷床 Earth - 06 War.mp3
